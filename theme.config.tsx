be10cdc804ac9862030dfb657ed8d7b103f284c7
chozr <github.eloquence305@passmail.com>
2025-06-11 01:22:08 -0400

    update config

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,14 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span className="font-semibold">Chozr7 Docs</span>,
-  project: {
-    link: 'https://github.com/chozr7/chozr7.github.io'
-  },
-  docsRepositoryBase: 'https://github.com/chozr7/chozr7.github.io',
-  footer: {
-    text: 'MIT © 2025-present Chozr7'
-  },
-  primaryHue: 200,
-  darkMode: true,
+    logo: <span style={{ fontWeight: 'bold' }}>Zen Development</span>,
+    project: {
+        link: 'https://github.com/chozr7/chozr7.github.io',
+    },
+    docsRepositoryBase: 'https://github.com/chozr7/chozr7.github.io',
+    footer: { component: () => null },
+    primaryHue: 150,
+    darkMode: true,
 }
 
 export default config
