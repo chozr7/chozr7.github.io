1260ea2e586d3aaf6b870792063a25497db197e3
chozr <github.eloquence305@passmail.com>
2025-06-10 23:58:46 -0400

    fix basePath

--- next.config.js
+++ next.config.js
@@ -1,18 +1,16 @@
 /** @type {import('next').NextConfig} */
 const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-  staticImage: true,
-  defaultShowCopyCode: true,
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.tsx',
+    staticImage: true,
+    defaultShowCopyCode: true,
 })
 
 const isGithubPages = process.env.GITHUB_ACTIONS === 'true'
 
 module.exports = withNextra({
-  images: {
-    unoptimized: isGithubPages,
-  },
-  basePath: isGithubPages ? '/chozr7.github.io' : '',
-  trailingSlash: true,
-  output: 'export',
+    images: { unoptimized: isGithubPages },
+    basePath: '',
+    trailingSlash: true,
+    output: 'export',
 })
